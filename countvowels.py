c3c35947b13ad462f72685305a8f3339ecdb8ae5
SUBASRI-D <subasridurai07@gmail.com>
2024-08-10 22:05:53 +0530

    commit change in countvowelsprogram

--- countvowels.py
+++ countvowels.py
@@ -1,6 +1,11 @@
 stringarr=input("Enter array string")
-targetval=input("enter your target")
-for i in range(len(stringarr)):
-    if stringarr[i]==targetval:
-        print(i)
+stringarr=stringarr.upper()
+stringarr1=list(stringarr)
+count=0
+for i in range(len(stringarr1)):
+    if(stringarr[i]=="A" or stringarr[i]=="E" or stringarr[i]=="I" or stringarr[i]=="O" or stringarr[i]=="U"):
+        count+=1
+print(count)
+        
+
 
